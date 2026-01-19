0298d154e7d8deeb9777eed0c2883330fc274aea
Humberto Zayas <soundsinhd@gmail.com>
2026-01-19 14:45:24 -0500

    updated to node 20.20.0

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,9 +13,9 @@ function Footer() {
         </h1>
 
         <div id="sb_instagram" className="sbi sbi_col_4" style={{ width: '100%', paddingBottom: '10px' }} data-id="801759764" data-num="8" data-res="auto" data-cols="4" data-options="{&quot;sortby&quot;: &quot;none&quot;, &quot;showbio&quot;: &quot;false&quot;, &quot;headercolor&quot;: &quot;ebebeb&quot;, &quot;imagepadding&quot;: &quot;5&quot;}" data-sbi-index="0">
-          <div class="form-normal-social" style={{ display: 'flex', width: '100%', justifyContent: 'center', marginTop: 0 }}>
-          <span class="text-span-29"></span>
-          <a href="https://instagram.com/frombelowstudio" target="_blank" rel="noreferrer" style={{ marginRight: '10px', marginTop: '-7px' }} class="link-12"><img style={{width: '32px'}} src={instagramIcon} alt='From Below Instagram' /></a> | <a style={{ marginRight: '10px', marginTop: '-7px', marginLeft: '10px' }} href="https://x.com/frombelowstudio" target="_blank" rel="noreferrer" class="link-5"><img style={{width: '31px'}} src={xIcon} alt="From Below X Twitter" />
+          <div className="form-normal-social" style={{ display: 'flex', width: '100%', justifyContent: 'center', marginTop: 0 }}>
+          <span className="text-span-29"></span>
+          <a href="https://instagram.com/frombelowstudio" target="_blank" rel="noreferrer" style={{ marginRight: '10px', marginTop: '-7px' }} className="link-12"><img style={{width: '32px'}} src={instagramIcon} alt='From Below Instagram' /></a> | <a style={{ marginRight: '10px', marginTop: '-7px', marginLeft: '10px' }} href="https://x.com/frombelowstudio" target="_blank" rel="noreferrer" className="link-5"><img style={{width: '31px'}} src={xIcon} alt="From Below X Twitter" />
 
           </a>
         </div>
